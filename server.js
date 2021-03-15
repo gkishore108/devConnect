f5999c86236c4de46a2d58cd95a94b8583e58fcf
Kishore <kishore260490@outlook.com>
2021-03-15 09:14:22 +0530

    added server api

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server Test");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
