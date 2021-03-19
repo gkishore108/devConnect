753d9196b137d95aa11242010bc6edae2f0f95e0
Kishore <kishore260490@outlook.com>
2021-03-19 18:38:22 +0530

    redux store init

--- client/src/Router.js
+++ client/src/Router.js
@@ -5,22 +5,27 @@ import Home from "./components/Home";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function Router() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/register'>
-          <Register />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
